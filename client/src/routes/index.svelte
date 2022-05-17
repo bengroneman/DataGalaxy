1a3db5d6120fb905bfbb29777110785d602922e2
bengroneman <bluegroneman10@gmail.com>
2022-05-17 12:35:13 -0600

    linting

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,19 +1,20 @@
 <script>
-  let modalOpen = false;
+	let modalOpen = false;
 </script>
-{#if modalOpen}
-  <div
-    id="newImageModal"
-    class="relative z-10"
-    aria-labelledby="new image modal"
-    role="dialog"
-    aria-modal="true"
-  >
-    <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
-    <div class="fixed z-10 inset-0 overflow-y-auto">
-      <div class="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
-        <!--
+{#if modalOpen}
+	<div
+		id="newImageModal"
+		class="relative z-10"
+		aria-labelledby="new image modal"
+		role="dialog"
+		aria-modal="true"
+	>
+		<div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+
+		<div class="fixed z-10 inset-0 overflow-y-auto">
+			<div class="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
+				<!--
           Modal panel, show/hide based on modal state.
 
           Entering: "ease-out duration-300"
@@ -23,77 +24,126 @@
             From: "opacity-100 translate-y-0 sm:scale-100"
             To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
         -->
-        <div class="relative bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full sm:p-6">
-          <div>
-            <div class="mt-3 text-center sm:mt-5">
-              <form name="newImageForm" action="http://localhost:8000/api/images/" target="_blank" method="POST" enctype="multipart/form-data">
-                  <div class="space-y-8 divide-y divide-gray-200">
-                    <div>
-                      <div>
-                        <h3 class="text-lg leading-6 font-medium text-gray-900">Add a new image</h3>
-                        <p class="mt-1 text-sm text-gray-500">This image will be displayd publicly so be careful what you share.</p>
-                      </div>
+				<div
+					class="relative bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full sm:p-6"
+				>
+					<div>
+						<div class="mt-3 text-center sm:mt-5">
+							<form
+								name="newImageForm"
+								action="http://localhost:8000/api/images/"
+								target="_blank"
+								method="POST"
+								enctype="multipart/form-data"
+							>
+								<div class="space-y-8 divide-y divide-gray-200">
+									<div>
+										<div>
+											<h3 class="text-lg leading-6 font-medium text-gray-900">Add a new image</h3>
+											<p class="mt-1 text-sm text-gray-500">
+												This image will be displayd publicly so be careful what you share.
+											</p>
+										</div>
 
-                      <div class="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
-                        <div class="sm:col-span-6">
-                          <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                            <div class="space-y-1 text-center">
-                              <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                                <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                              </svg>
-                              <div class="flex text-sm text-gray-600">
-                                <label for="image" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                                  <span>Upload a file</span>
-                                  <input id="image" name="image" type="file" class="sr-only">
-                                </label>
-                                <p class="pl-1">or drag and drop</p>
-                              </div>
-                              <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                <div class="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
-                  <button
-                      type="submit"
-                      class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm"
-                  >Save</button>
-                  <button
-                      type="button"
-                      on:click={() => modalOpen = false}
-                      class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm"
-                  >Cancel</button>
-                </div>
-              </form>
-            </div>
-          </div>
-
-        </div>
-      </div>
-    </div>
-  </div>
+										<div class="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+											<div class="sm:col-span-6">
+												<div
+													class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md"
+												>
+													<div class="space-y-1 text-center">
+														<svg
+															class="mx-auto h-12 w-12 text-gray-400"
+															stroke="currentColor"
+															fill="none"
+															viewBox="0 0 48 48"
+															aria-hidden="true"
+														>
+															<path
+																d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+																stroke-width="2"
+																stroke-linecap="round"
+																stroke-linejoin="round"
+															/>
+														</svg>
+														<div class="flex text-sm text-gray-600">
+															<label
+																for="image"
+																class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+															>
+																<span>Upload a file</span>
+																<input id="image" name="image" type="file" class="sr-only" />
+															</label>
+															<p class="pl-1">or drag and drop</p>
+														</div>
+														<p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
+									<button
+										type="submit"
+										class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm"
+										>Save</button
+									>
+									<button
+										type="button"
+										on:click={() => (modalOpen = false)}
+										class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm"
+										>Cancel</button
+									>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 {/if}
 
 <div class="text-center mt-12 pt-12">
-  <svg class="mx-auto h-12 w-12 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-    <path vector-effect="non-scaling-stroke" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z" />
-  </svg>
-  <h3 class="mt-2 text-sm font-medium text-gray-900">No images</h3>
-  <p class="mt-1 text-sm text-gray-500">Get started by adding a new image.</p>
-  <div class="mt-6">
-    <button
-        type="button"
-        on:click={() => modalOpen = true}
-        id="newImageButton"
-        class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-    >
-      <!-- Heroicon name: solid/plus -->
-      <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-        <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" />
-      </svg>
-      Add Image
-    </button>
-  </div>
+	<svg
+		class="mx-auto h-12 w-12 text-gray-400"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+		aria-hidden="true"
+	>
+		<path
+			vector-effect="non-scaling-stroke"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="2"
+			d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
+		/>
+	</svg>
+	<h3 class="mt-2 text-sm font-medium text-gray-900">No images</h3>
+	<p class="mt-1 text-sm text-gray-500">Get started by adding a new image.</p>
+	<div class="mt-6">
+		<button
+			type="button"
+			on:click={() => (modalOpen = true)}
+			id="newImageButton"
+			class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+		>
+			<!-- Heroicon name: solid/plus -->
+			<svg
+				class="-ml-1 mr-2 h-5 w-5"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 20 20"
+				fill="currentColor"
+				aria-hidden="true"
+			>
+				<path
+					fill-rule="evenodd"
+					d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+					clip-rule="evenodd"
+				/>
+			</svg>
+			Add Image
+		</button>
+	</div>
 </div>
