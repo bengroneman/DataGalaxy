411db072882724c48bbcc1c3509ba1b6474b76b9
Ben Groneman <hello@bluedojo.dev>
2022-05-18 10:53:24 -0600

    production env variables

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,10 +1,17 @@
 import adapter from '@sveltejs/adapter-netlify';
+import dotenv from 'dotenv';
+import preprocess from 'svelte-preprocess';
+
+dotenv.config();
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	compilerOptions: {
 		hydratable: true
 	},
+	preprocess: preprocess({
+		replace: [["process.env.MODE", process.env.MODE]],
+	}),
 	kit: {
 		adapter: adapter(),
 		trailingSlash: 'always',
