960eae76fb1925d811bc6c231318adaa6fb75033
Ben Groneman <hello@bluedojo.dev>
2022-05-18 09:59:29 -0600

    additional tests for images

--- client/tests/test.js
+++ client/tests/test.js
@@ -1,13 +1,40 @@
 import { expect, test } from '@playwright/test';
 
 test('adding a PNG is successful', async ({ page, request }) => {
-	await page.goto('/');
+	page.on('load', async () => {
+		await page.locator('button.open-modal').click();
+		await page.locator('label.input-file');
+		page.on('filechooser', async (fileChooser) => {
+			await fileChooser.setFiles('../assets/100x400.png');
+		});
 
-	await page.locator("button.open-modal").click();
+		await page.locator('#newImageForm .save-image').click();
+		expect(await request.response.status).toBe(200);
+	});
+});
 
-	await page.type("form.new-image-form .file-input", '../assets.100x400.png');
+test('adding a PDF fails', async ({ page, request }) => {
+	page.on('load', async () => {
+		await page.locator('button.open-modal').click();
+		await page.locator('label.input-file');
+		page.on('filechooser', async (fileChooser) => {
+			await fileChooser.setFiles('../assets/Portfolio.pdf');
+		});
 
-	await page.locator("#newImageForm .save-image").click();
+		await page.locator('#newImageForm .save-image').click();
+		expect(await request.response.status).toBe(400);
+	});
+});
 
-	expect(request.failure().errorText).toBe(false);
+test('adding a file over 10mb fails', async ({ page, request }) => {
+	page.on('load', async () => {
+		await page.locator('button.open-modal').click();
+		await page.locator('label.input-file');
+		page.on('filechooser', async (fileChooser) => {
+			await fileChooser.setFiles('../assets/10mb.png');
+		});
+
+		await page.locator('#newImageForm .save-image').click();
+		expect(await request.response.status).toBe(400);
+	});
 });
