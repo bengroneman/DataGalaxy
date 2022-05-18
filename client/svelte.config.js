cf2da4fe65342b52c99397b65fd8be35d649003a
bengroneman <bluegroneman10@gmail.com>
2022-05-18 12:05:26 -0600

    fix: no need for .env

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,26 +1,16 @@
 import adapter from '@sveltejs/adapter-netlify';
-import dotenv from 'dotenv';
-import preprocess from 'svelte-preprocess';
-
-dotenv.config();
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	compilerOptions: {
 		hydratable: true
 	},
-	preprocess: preprocess({
-		replace: [["process.env.VITE_MODE", process.env.VITE_MODE]],
-	}),
 	kit: {
 		adapter: adapter(),
 		trailingSlash: 'always',
 		prerender: {
 			default: true
 		}
-		plugins: [
-			replace
-		]
 	},
 };
 
