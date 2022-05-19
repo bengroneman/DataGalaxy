2fc0568cbb6c0cb50aaa06f9ed0535a2b284891c
Ben Groneman <hello@bluedojo.dev>
2022-05-19 16:16:07 -0600

    loading icon - baseUrl bug fix

--- client/src/components/icons/LoadingIcon.svelte
+++ client/src/components/icons/LoadingIcon.svelte
@@ -1,9 +1,14 @@
-<script>
-	export let width = 6;
-	export let height = 6;
-</script>
-
 <svg
-  xmlns="http://www.w3.org/2000/svg" class="animate-spin z-20 absolute -mt-8 h-{height} w-{width}" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+	xmlns="http://www.w3.org/2000/svg"
+	class="animate-spin z-20 -mt-8 h-6 w-6"
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke="currentColor"
+	stroke-width="2"
+>
+	<path
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+	/>
 </svg>
